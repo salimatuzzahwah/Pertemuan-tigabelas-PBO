a386ad034fd2a2abe04d4a89a3379d7192a235a8
Elma <salimaatuz06@gmail.com>
2025-11-12 14:37:52 +0700

    Add files via upload

---
+++ pertemuan13/build/generated-sources/ap-source-output/pertemuan13/Login_1_.java
@@ -0,0 +1,14 @@
+package pertemuan13;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-11T17:53:50", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Login_1.class)
+public class Login_1_ { 
+
+    public static volatile SingularAttribute<Login_1, String> password;
+    public static volatile SingularAttribute<Login_1, String> username;
+
+}
